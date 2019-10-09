ee6203513b733beb0dfba9327dcfb9ba5f6a9743
Faysal BOUGAMALE <fbougama@e1r4p13.42.fr>
2019-10-09 16:42:36 +0200

    maj_strlen

--- libft/ft_strlen.c
+++ libft/ft_strlen.c
@@ -6,9 +6,11 @@
 /*   By: fbougama <fbougama@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/07 10:42:43 by fbougama          #+#    #+#             */
-/*   Updated: 2019/10/07 14:01:41 by fbougama         ###   ########.fr       */
+/*   Updated: 2019/10/09 16:42:14 by fbougama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stdio.h>
 
 size_t	ft_strlen(const char *str)
 {
