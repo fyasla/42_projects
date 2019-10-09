06c4f53cd3f4325313b09b7333b48d07bb20b8b9
Faysal BOUGAMALE <fbougama@e1r4p13.42.fr>
2019-10-09 15:41:24 +0200

    memmove_main

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -6,25 +6,27 @@
 /*   By: fbougama <fbougama@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/07 14:47:08 by fbougama          #+#    #+#             */
-/*   Updated: 2019/10/08 14:00:52 by fbougama         ###   ########.fr       */
+/*   Updated: 2019/10/09 15:40:58 by fbougama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stdio.h>
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
 	const char	*s;
 	char		*d;
-	int			i;
 	char		*end_s;
 	char		*end_d;
 
-	i = 0;
-	end_s = s + len - 1;
-	end_d = d + len - 1;
+	s = src;
+	d = dst;
+	end_s = (char*)s + (len - 1);
+	end_d = d + (len - 1);
 	if (d < s)
 	{
-		while (i++ < len)
-			d++ = s++;
+		while (len-- > 0)
+			*d++ = *s++;
 	}
 	else
 	{
