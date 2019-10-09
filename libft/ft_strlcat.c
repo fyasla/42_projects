eaedcde998a77a1f63586b1851bb32a157eaf98d
Faysal BOUGAMALE <fbougama@e1r4p13.42.fr>
2019-10-09 19:38:53 +0200

    ft_strlcat

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -6,8 +6,29 @@
 /*   By: fbougama <fbougama@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/09 10:56:11 by fbougama          #+#    #+#             */
-/*   Updated: 2019/10/09 10:58:44 by fbougama         ###   ########.fr       */
+/*   Updated: 2019/10/09 19:38:39 by fbougama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-size_t	ft_strlcat(char *dst,  const char *src, size_t dstsize)
+#include <stdio.h>
+
+size_t	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+{
+	size_t	src_len;
+	size_t	dst_len;
+
+	src_len = ft_strlen(src);
+	dst_len = ft_strlen(dst);
+
+	return (src_len + dst_len);
+}
