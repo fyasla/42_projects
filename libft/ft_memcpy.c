0f66435850a0fcaec8aa33744b41c7d26ca4d6d0
Faysal BOUGAMALE <fbougama@e1r4p13.42.fr>
2019-10-09 12:44:11 +0200

    memcpy_main

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -6,9 +6,11 @@
 /*   By: fbougama <fbougama@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/07 14:03:40 by fbougama          #+#    #+#             */
-/*   Updated: 2019/10/08 14:54:03 by fbougama         ###   ########.fr       */
+/*   Updated: 2019/10/09 12:37:27 by fbougama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stdio.h>
 
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
@@ -17,7 +19,7 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 
 	s = src;
 	cpy = dst;
-	while (n--)
+	while (n-- > 0)
 		*cpy++ = *s++;
 	return (dst);
 }
