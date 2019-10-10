0c509cd0eb702ac77f36ba632500018b99ec2092
Faysal BOUGAMALE <fbougama@e1r4p13.42.fr>
2019-10-10 11:42:43 +0200

    ft_strlcat_pb

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -6,13 +6,13 @@
 /*   By: fbougama <fbougama@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/09 10:56:11 by fbougama          #+#    #+#             */
-/*   Updated: 2019/10/09 19:38:39 by fbougama         ###   ########.fr       */
+/*   Updated: 2019/10/10 11:32:07 by fbougama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
-size_t	ft_strlen(char *str)
+static size_t	ft_strlen(char *str)
 {
 	int	i;
 
@@ -22,13 +22,31 @@ size_t	ft_strlen(char *str)
 	return (i);
 }
 
-size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+static void		*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	char		*d;
+	const char	*s;
+
+	d = dst;
+	s = src;
+	while (n-- > 0)
+		*d++ = *s++;
+	return (dst);
+}
+
+size_t			ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
 	size_t	src_len;
 	size_t	dst_len;
 
 	src_len = ft_strlen(src);
 	dst_len = ft_strlen(dst);
-
+	if (src_len + dst_len < dstsize)
+		ft_memcpy(dst + dst_len, src, src_len + 1);
+	else
+	{
+		ft_memcpy(dst + dst_len, src, dstsize - dst_len - 1);
+		dst[dst_len + dstsize] = '\0';
+	}
 	return (src_len + dst_len);
 }
