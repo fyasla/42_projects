a7cb7cc2539c0eca98d58a9065bc8d8524d40241
Faysal BOUGAMALE <fbougama@e1r4p13.42.fr>
2019-10-10 14:18:42 +0200

    ft_strdup

---
+++ libft/ft_strdup.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fbougama <fbougama@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/10 14:02:04 by fbougama          #+#    #+#             */
+/*   Updated: 2019/10/10 14:18:27 by fbougama         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+static void		*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	char		*d;
+	const char	*s;
+
+	d = (char*)dst;
+	s = (const char *)src;
+	while (n-- > 0)
+		*d++ = *s++;
+	return (dst);
+}
+
+static size_t	ft_strlen(const char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char			*ft_strdup(const char *s1)
+{
+	size_t	len;
+	char	*ptr;
+
+	len = ft_strlen(s1) + 1;
+	if (!(ptr = malloc(len)))
+		return (NULL);
+	ft_memcpy(ptr, s1, len + 1);
+	return (ptr);
+}
