ede845d56aec7ee65075daafcbd46c03c9c2a2eb
Faysal BOUGAMALE <fbougama@e2r4p13.42.fr>
2019-10-15 16:40:38 +0200

    maj_strjoin

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -6,22 +6,13 @@
 /*   By: fbougama <fbougama@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/10 15:31:52 by fbougama          #+#    #+#             */
-/*   Updated: 2019/10/11 12:24:38 by fbougama         ###   ########.fr       */
+/*   Updated: 2019/10/15 16:39:26 by fbougama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
-
-size_t	ft_strlen(const char *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i])
-		i++;
-	return (i);
-}
+#include "libft.h"
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
@@ -33,15 +24,12 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	i = 0;
 	l1 = ft_strlen(s1);
 	l2 = ft_strlen(s2);
-	printf("bjr");
 	if (!(ptr = malloc((l1 + l2 + 1) * sizeof(char))))
 		return (NULL);
-	printf("bjr");
 	while (l1-- > 0)
 		ptr[i++] = *s1++;
 	while (l2-- > 0)
 		ptr[i++] = *s2++;
-	printf("bjr");
 	ptr[i++] = '\0';
 	return (ptr);
 }
