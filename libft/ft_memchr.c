131e7825e8bd9ab0390f6323ee2f4c18fcf51d79
Faysal BOUGAMALE <fbougama@e2r4p13.42.fr>
2019-10-15 16:21:36 +0200

    maj_memchr

--- libft/ft_memchr.c
+++ libft/ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: fbougama <fbougama@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/07 15:35:35 by fbougama          #+#    #+#             */
-/*   Updated: 2019/10/15 13:43:47 by fbougama         ###   ########.fr       */
+/*   Updated: 2019/10/15 16:21:15 by fbougama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,15 @@
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	char	*str;
-	size_t	i;
+	unsigned char	*str;
+	size_t			i;
 
-	str = (char*)s;
+	str = (unsigned char*)s;
 	i = 0;
-	while (i < n && str[i] && str[i] != (unsigned char)c)
+	while (i < n && *(str + i) != '\0' && *(str + i) != (unsigned char)c)
 		i++;
-	if (i == n || !str[i])
+	if (i == n || *(str + i) == '\0')
 		return (NULL);
 	else
-		return (str + i);
+		return ((void*)s + i);
 }
