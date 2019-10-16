943b9cd04930b8df70471ab0cf5b8353e6e19233
Faysal BOUGAMALE <fbougama@e2r4p16.42.fr>
2019-10-16 19:03:30 +0200

    maj_lstmap

--- libft/ft_lstmap_bonus.c
+++ libft/ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: fbougama <fbougama@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/16 12:42:29 by fbougama          #+#    #+#             */
-/*   Updated: 2019/10/16 12:42:34 by fbougama         ###   ########.fr       */
+/*   Updated: 2019/10/16 19:03:02 by fbougama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,8 @@ t_list			*ft_lstmap(t_list *lst, void *(*f)(void *))
 
 	if (!(cpy_start = (t_list**)malloc(sizeof(cpy_start))))
 		return (NULL);
+	*cpy_start = ft_lstnew((*f)(lst->content));
+	lst = lst->next;
 	while (lst != NULL)
 	{
 		content = lst->content;
