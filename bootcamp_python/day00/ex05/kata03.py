0a8c48bdc86c4e4a334c858f3adf7c9c299ebd5c
Faysal BOUGAMALE <fbougama@e1r4p14.42.fr>
2019-11-04 16:36:05 +0100

    ex06

--- bootcamp_python/day00/ex05/kata03.py
+++ bootcamp_python/day00/ex05/kata03.py
@@ -1,1 +1,14 @@
+import sys
+
 phrase = "The right format"
+
+def print42(str):
+	ajust = ""
+	if len(str) > 42 :
+		sys.stdout.write(str)
+	else :
+		for i in range (42 - len(str)):
+			ajust+='-'
+	sys.stdout.write(ajust + str)
+
+print42(phrase)
---
+++ bootcamp_python/day00/ex07/filterwords.py
@@ -0,0 +1,2 @@
+def filterword(list, n):
+	
