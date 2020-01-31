69ac6cb5130c990fe5264f645f5c113d87fcd7fb
Faysal BOUGAMALE <fbougama@e2r3p14.42.fr>
2020-01-31 18:01:46 +0100

    Makefiles : ok

--- miniRT/tests/compil_parsing_test.sh
+++
@@ -1,7 +0,0 @@
-gcc -o parsing_test -Wall -Werror -Wextra ../src/parsing/parse_1.c \
-../src/parsing/parse_2.c \
-../src/parsing/parsing_utils.c ../src/parsing/parsing_utils2.c \
-../src/parsing/parsing_utils3.c ../src/parsing/initiate_scene.c \
-../src/parsing/map_parse.c ../src/parsing/get_next_line_utils.c \
-../libftprintf.a ../tests/src/parsing_display.c \
-../src/parsing/get_next_line.c ../tests/src/parsing_test.c
