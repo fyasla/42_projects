3ace1e5ee1bed335486089d9da04f555d9754f71
Faysal BOUGAMALE <fbougama@e2r3p10.42.fr>
2020-02-05 15:43:30 +0100

    trinome.c

--- miniRT/inc/minirt.h
+++ miniRT/inc/minirt.h
@@ -6,7 +6,7 @@
 /*   By: fbougama <fbougama@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/07 10:37:08 by fbougama          #+#    #+#             */
-/*   Updated: 2020/02/05 15:25:38 by fbougama         ###   ########.fr       */
+/*   Updated: 2020/02/05 15:41:59 by fbougama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,5 +54,9 @@ t_vec3	cam_to_world(t_vec3 cam_crd, t_cam cam);
 t_ray	pix_ray(t_pix pix, t_scene *scene, int cam);
 
 int		draw(t_win *win, t_scene *scene_ptr, int cam);
+
+double	discriminant(t_vec3 tri);
+int		nb_pos_sol(t_vec3 tri);
+double	smallest_sol(t_vec3 tri);
 
 #endif
---
+++ miniRT/src/math_utils/trinome.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   trinome.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fbougama <fbougama@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/05 15:27:57 by fbougama          #+#    #+#             */
+/*   Updated: 2020/02/05 15:43:07 by fbougama         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../inc/minirt.h"
+
+double	discriminant(t_vec3 tri)
+{
+	return(tri.y * tri.y - 4 * tri.x * tri.z);
+}
+
+int		nb_pos_sol(t_vec3 tri)
+{
+	int		nb;
+	double	sol;
+	double delta;
+
+	nb = 0;
+	delta = discriminant(tri);
+	if (delta < 0)
+		return (0);
+	sol = (-tri.y - sqrt(delta)) / (2 * tri.x);
+	if (sol >= 0)
+		nb += 1;;
+	sol = (-tri.y + sqrt(delta)) / (2 * tri.x);
+	if (sol >= 0)
+		nb += 1;
+	return (nb);
+}
+
+double	smallest_sol(t_vec3 tri)
+{
+	double	sol;
+	double delta;
+
+	delta = discriminant(tri);
+	if (delta < 0)
+		return (-1);
+	sol = (-tri.y - sqrt(delta)) / (2 * tri.x);
+	if (sol >= 0)
+		return (sol);
+	sol = (-tri.y + sqrt(delta)) / (2 * tri.x);
+	if (sol >= 0)
+		return (sol);
+	else
+		return (-1);
+}
