f68bbed57666cebf3314501da6bd51d4645cd7ac
fafays <f.bougamale@laposte.net>
2020-06-25 16:28:54 +0200

    starting to norminate

--- miniRT/src/base/save_main.c
+++ miniRT/src/base/save_main.c
@@ -6,124 +6,90 @@
 /*   By: faysal <faysal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/22 19:41:42 by faysal            #+#    #+#             */
-/*   Updated: 2020/05/23 15:40:42 by faysal           ###   ########.fr       */
+/*   Updated: 2020/06/25 16:27:39 by faysal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minirt.h"
 
-const int bytesPerPixel = 3; /// red, green, blue
-const int fileHeaderSize = 14;
-const int infoHeaderSize = 40;
+int				gen(t_scene *scene)
+{
+	int				h;
+	int				w;
+	unsigned char	image[scene->resy][scene->resx][3];
+	char			*imgfn = "bitmapImage.bmp";
+	t_pix			p;
 
-void generateBitmapImage(unsigned char *image, int height, int width, char* imageFileName);
-unsigned char* createBitmapFileHeader(int height, int width, int paddingSize);
-unsigned char* createBitmapInfoHeader(int height, int width);
-
-
-int gen(t_scene *scene){
-    int height = scene->resy;
-    int width = scene->resx;
-    unsigned char image[height][width][bytesPerPixel];
-    char* imageFileName = "bitmapImage.bmp";
-
-    t_pix	p;
-
-    p.x = 0;
-    while(p.x < width){
-        p.y = 0;
-        while(p.y < height){
-            image[height - 1 - p.y][p.x][2] = (unsigned char)pix_col(p, scene, scene->cam).r; ///red
-            image[height - 1 - p.y][p.x][1] = (unsigned char)pix_col(p, scene, scene->cam).g; ///green
-            image[height - 1 - p.y][p.x][0] = (unsigned char)pix_col(p, scene, scene->cam).b; ///blue
-            p.y++;
-        }
-        p.x++;
-    }
-
-
-    generateBitmapImage((unsigned char *)image, height, width, imageFileName);
-    return(0);
+	p.x = 0;
+	h = scene->resy;
+	w = scene->resx;
+	while (p.x < w)
+	{
+		p.y = 0;
+		while (p.y < h)
+		{
+			image[h - 1 - p.y][p.x][2] = (unsigned char)pix_col(p, scene, scene->cam).r;
+			image[h - 1 - p.y][p.x][1] = (unsigned char)pix_col(p, scene, scene->cam).g;
+			image[h - 1 - p.y][p.x][0] = (unsigned char)pix_col(p, scene, scene->cam).b;
+			p.y++;
+		}
+		p.x++;
+	}
+	genbmpim((unsigned char *)image, h, w, imgfn);
+	return(0);
 }
 
+void			genbmpim(unsigned char *image, int h, int w, char *imgfn)
+{
+	unsigned char	padding[3] = {0, 0, 0};
+	int				padds = (4 - (w * 3) % 4) % 4;
+	unsigned char	*fileh = createbmpfh(h, w, padds);
+	unsigned char	*infoh = createbmpih(h, w);
+	int				imgf;
 
-void generateBitmapImage(unsigned char *image, int height, int width, char* imageFileName){
-
-    unsigned char padding[3] = {0, 0, 0};
-    int paddingSize = (4 - (width*bytesPerPixel) % 4) % 4;
-
-    unsigned char* fileHeader = createBitmapFileHeader(height, width, paddingSize);
-    unsigned char* infoHeader = createBitmapInfoHeader(height, width);
-
-    // FILE* imageFile = fopen(imageFileName, "wb");
-    int imageFile;
-    imageFile = open(imageFileName, O_WRONLY);    
-
-    /* fwrite(fileHeader, 1, fileHeaderSize, imageFile);
-    fwrite(infoHeader, 1, infoHeaderSize, imageFile); */
-
-    write(imageFile, fileHeader, fileHeaderSize);
-    write(imageFile, infoHeader, infoHeaderSize);
-    int i;
-    i = 0;
-    while(i < height){
-       /*  fwrite(image+(i*width*bytesPerPixel), bytesPerPixel, width, imageFile);
-        fwrite(padding, 1, paddingSize, imageFile); */
-        write(imageFile, image+(i*width*bytesPerPixel), bytesPerPixel * width);
-        write(imageFile, padding, paddingSize);
-        i++;
-    }
-
-    close(imageFile);
+	imgf = open(imgfn, O_WRONLY);
+	write(imgf, fileh, 14);
+	write(imgf, infoh, 40);
+	int i;
+	i = 0;
+	while (i < h)
+	{
+		write(imgf, image + (i * w * 3), 3 * w);
+		write(imgf, padding, padds);
+		i++;
+	}
+	close(imgf);
 }
 
-unsigned char* createBitmapFileHeader(int height, int width, int paddingSize){
-    int fileSize = fileHeaderSize + infoHeaderSize + (bytesPerPixel*width+paddingSize) * height;
+unsigned char	*createbmpfh(int h, int w, int padds)
+{
+	int						files = 14 + 40 + (3 * w + padds) * h;
+	static unsigned char	fileh[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
-    static unsigned char fileHeader[] = {
-        0,0, /// signature
-        0,0,0,0, /// image file size in bytes
-        0,0,0,0, /// reserved
-        0,0,0,0, /// start of pixel array
-    };
-
-    fileHeader[ 0] = (unsigned char)('B');
-    fileHeader[ 1] = (unsigned char)('M');
-    fileHeader[ 2] = (unsigned char)(fileSize    );
-    fileHeader[ 3] = (unsigned char)(fileSize>> 8);
-    fileHeader[ 4] = (unsigned char)(fileSize>>16);
-    fileHeader[ 5] = (unsigned char)(fileSize>>24);
-    fileHeader[10] = (unsigned char)(fileHeaderSize + infoHeaderSize);
-
-    return fileHeader;
+	fileh[0] = (unsigned char)('B');
+	fileh[1] = (unsigned char)('M');
+	fileh[2] = (unsigned char)(files);
+	fileh[3] = (unsigned char)(files >> 8);
+	fileh[4] = (unsigned char)(files >> 16);
+	fileh[5] = (unsigned char)(files >> 24);
+	fileh[10] = (unsigned char)(14 + 40);
+	return (fileh);
 }
 
-unsigned char* createBitmapInfoHeader(int height, int width){
-    static unsigned char infoHeader[] = {
-        0,0,0,0, /// header size
-        0,0,0,0, /// image width
-        0,0,0,0, /// image height
-        0,0, /// number of color planes
-        0,0, /// bits per pixel
-        0,0,0,0, /// compression
-        0,0,0,0, /// image size
-        0,0,0,0, /// horizontal resolution
-        0,0,0,0, /// vertical resolution
-        0,0,0,0, /// colors in color table
-        0,0,0,0, /// important color count
-    };
+unsigned char	*createbmpih(int h, int w)
+{
+	static unsigned char	infoh[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
-    infoHeader[ 0] = (unsigned char)(infoHeaderSize);
-    infoHeader[ 4] = (unsigned char)(width    );
-    infoHeader[ 5] = (unsigned char)(width>> 8);
-    infoHeader[ 6] = (unsigned char)(width>>16);
-    infoHeader[ 7] = (unsigned char)(width>>24);
-    infoHeader[ 8] = (unsigned char)(height    );
-    infoHeader[ 9] = (unsigned char)(height>> 8);
-    infoHeader[10] = (unsigned char)(height>>16);
-    infoHeader[11] = (unsigned char)(height>>24);
-    infoHeader[12] = (unsigned char)(1);
-    infoHeader[14] = (unsigned char)(bytesPerPixel*8);
-
-    return infoHeader;
+	infoh[0] = (unsigned char)(40);
+	infoh[4] = (unsigned char)(w);
+	infoh[5] = (unsigned char)(w >> 8);
+	infoh[6] = (unsigned char)(w >> 16);
+	infoh[7] = (unsigned char)(w >> 24);
+	infoh[8] = (unsigned char)(h);
+	infoh[9] = (unsigned char)(h >> 8);
+	infoh[10] = (unsigned char)(h >> 16);
+	infoh[11] = (unsigned char)(h >> 24);
+	infoh[12] = (unsigned char)(1);
+	infoh[14] = (unsigned char)(3 * 8);
+	return (infoh);
 }
--- miniRT/src/main.c
+++ miniRT/src/main.c
@@ -6,7 +6,7 @@
 /*   By: faysal <faysal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/07 10:15:03 by fbougama          #+#    #+#             */
-/*   Updated: 2020/06/17 15:32:49 by faysal           ###   ########.fr       */
+/*   Updated: 2020/06/24 18:18:41 by faysal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,15 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n)
 	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
 
+int		check_args(int ac, int map_fd, char **av)
+{
+	if (ac != 2 && (ac != 3 || ft_strncmp(av[2], "-save", 6) != 0))
+		ft_printf("Arguments should be one map (and optionnaly \"-save\")\n");
+	if (map_fd == -1)
+		ft_printf(ft_strjoin("ERROR while opening ", av[1]));
+	return (0);
+}
+
 int		main(int ac, char **av)
 {
 	t_win	win;
@@ -31,16 +40,8 @@ int		main(int ac, char **av)
 	int		map_fd;
 	char	*img;
 
-	if (ac != 2 && (ac != 3 || ft_strncmp(av[2], "-save", 6) != 0))
-	{
-		ft_printf("Arguments should be one map (and optionnaly \"-save\")\n");
-		return (0);
-	}
-	if ((map_fd = open(av[1], O_RDONLY)) == -1)
-	{
-		ft_printf(ft_strjoin("ERROR while opening ", av[1]));
-		return (0);
-	};
+	map_fd = open(av[1], O_RDONLY);
+	check_args(ac, map_fd, av);
 	scene = map_parse(map_fd);
 	close(map_fd);
 	win.mlx_p = mlx_init();
@@ -49,8 +50,6 @@ int		main(int ac, char **av)
 	img = create_img(&win, scene);
 	if (ac == 3 && ft_strncmp(av[2], "-save", 6) == 0)
 		gen(scene);
-	//draw(&win, scene, img);
-	//mlx_hook(win.win_p, 1, 1L << 8, &exit_prog, &win);
 	mlx_key_hook(win.win_p, deal_key, &win);
 	mlx_hook(win.win_p, 17, 131072, exit_prog, &win);
 	draw(&win, scene, img);
