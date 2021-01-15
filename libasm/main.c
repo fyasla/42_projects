a137ffb08732a2c207ad7cf2e373214c14f53ceb
fafays <f.bougamale@laposte.net>
2021-01-15 07:46:54 +0100

    go to 42

--- libasm/main.c
+++ libasm/main.c
@@ -6,18 +6,19 @@
 /*   By: faysal <faysal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/12 16:09:05 by faysal            #+#    #+#             */
-/*   Updated: 2021/01/14 18:12:42 by faysal           ###   ########.fr       */
+/*   Updated: 2021/01/15 00:05:09 by faysal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
 #include <stdio.h>
+#include "inc/libasm.h"
 
 int		main(int ac, char **av)
 {
 	char	*str;
 	(void)	ac;
 
-	printf("\nac : %d\nstrlen :%d", strlen(av[1]), ft_strlen(av[1]));
+	printf("ac : %d\nstrlen :%d\n", strlen(av[1]), ft_strlen(av[1]));
 	return (0);
 }
