c65d02d4479b9baa1f187c736864f78ceb0c4bc4
fafays <f.bougamale@laposte.net>
2021-01-16 03:34:49 +0100

    ft_write is ok

--- libasm/srcs/ft_write.s
+++ libasm/srcs/ft_write.s
@@ -2,12 +2,21 @@
 ; %rax	| System call	|		%rdi		| %rsi		| %rdx
 ;  0	| sys_write		| unsigned int fd	| char* buf	| size_t count
 
-extern __errno_location
 section .text
+	extern __errno_location
 	global ft_write
 
 ft_write:
-	mov rax, 1
-	syscall		;write (fd, buf, count) as rax = 1
- 	cmp     rax, 0
-  	ret
+	mov		rax, 1
+	syscall
+	cmp		rax, 0
+	jl		_error ; if there is an error
+	ret
+
+_error:
+	mov		rcx, rax
+	call	__errno_location	; so rax is pointing to errno
+	neg		rcx					; as the man says : "Valid error numbers are all positive numbers"
+	mov		[rax], rcx			; update errno value
+	mov		rax, -1				; return -1 
+	ret
