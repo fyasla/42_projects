39af78f1fadb40cf17c24c189ec02d9ee6eebe92
fafays <f.bougamale@laposte.net>
2021-01-16 01:02:50 +0100

    ft_strcpy : ok

--- libasm/srcs/ft_strcpy.s
+++ libasm/srcs/ft_strcpy.s
@@ -6,4 +6,16 @@ section .text
 	global ft_strcpy
 
 ft_strcpy:
-mov rax, rdi
+	cmp	rdi, 0	; check if pointer is not NULL
+	je _end
+	mov rcx, 0	
+
+_loop:
+	mov dl, BYTE[rsi + rcx]		; copy the char in buffer
+	mov BYTE[rdi + rcx], dl		; copy the char in dest
+	inc rcx						; increment
+	cmp dl, 0					; if char copied was 0 then end
+	jne _loop
+
+_end:
+	ret
