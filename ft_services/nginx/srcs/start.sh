92c502351039b922377ae81d3428b20f18c8a5dd
fafays <f.bougamale@laposte.net>
2021-06-12 20:16:54 +0200

    let's go

---
+++ ft_services/nginx/srcs/start.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+mv /srcs/nginx.conf /etc/nginx/nginx.conf
+
+adduser -D -g 'www' www && \
+mkdir /www && \
+mv /srcs/index.html /www/index.html && \
+chown -R www:www /var/lib/nginx && \
+chown -R www:www /www && \
+mkdir -p /run/nginx
+
+openrc
+touch /run/openrc/softlevel
+nginx -g 'daemon off;'
