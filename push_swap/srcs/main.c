58f479d0d3633af1d9e64b4154c54258fa573cde
Faysal BOUGAMALE <fbougama@e1r13p11.42.fr>
2021-07-09 17:36:15 +0200

    libft added into repo

--- push_swap/srcs/main.c
+++ push_swap/srcs/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: faysal <faysal@student.42.fr>              +#+  +:+       +#+        */
+/*   By: fbougama <fbougama@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/21 12:27:27 by fbougama          #+#    #+#             */
-/*   Updated: 2021/06/24 22:16:13 by faysal           ###   ########.fr       */
+/*   Updated: 2021/07/09 16:21:16 by fbougama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,18 +24,18 @@ int	main(int ac, char **av)
 	if(!(stacks = initiate_stacks(ac - 1, &av[1])))
 		return (-1);
 	print_stack(stacks->a);
-	printf("\nb:\n");
-	printf("\ntaille : %ld\n", sizeof(t_list2 **));
-	printf("\ntaille : %ld\n", sizeof(t_list2 *));
-	print_stack(stacks->b);
+	// printf("\nb:\n");
+	// printf("\ntaille : %ld\n", sizeof(t_list2 **));
+	// printf("\ntaille : %ld\n", sizeof(t_list2 *));
+	// print_stack(stacks->b);
 	swap(stacks->a);
-	push_ab(stacks->a, stacks->b);
-	push_ab(stacks->a, stacks->b);
-	swap(stacks->b);
+	// push_ab(stacks->a, stacks->b);
+	// push_ab(stacks->a, stacks->b);
+	// swap(stacks->b);
 
 	printf("\nafter\n");
 	print_stack(stacks->a);
-	printf("\nb:\n");
-	print_stack(stacks->b);
+	// printf("\nb:\n");
+	// print_stack(stacks->b);
 	return (0);
 }
