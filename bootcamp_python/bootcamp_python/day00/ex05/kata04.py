514977025cb498cb5a13503e4c9dae951f220f25
fyasla <f.bougamale@gmail.com>
2023-03-19 18:34:25 +0100

    cleaning

--- bootcamp_python/bootcamp_python/day00/ex05/kata04.py
+++
@@ -1,8 +0,0 @@
-t = (0, 4, 132.42222, 10000, 12345.67)
-
-def printkata04(tup):
-	print("day_" + str(t[0] / 10) + str(t[0] % 10) + ", " +
-	"ex_" + str(t[1] / 10) + str(t[1] % 10) + " : " +
-	str(round(t[2], 2)) + ", %.2e"%t[3] + ", %.2e"%t[4])
-	
-printkata04(t)
